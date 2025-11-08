4447c57d4b23fb2c7fa8e731c67ec76c67abae8e
bhavyalboddu <bhavyalboddu@gmail.com>
2025-11-08 18:10:43 -0500

    login page changed to index.php

--- index.php
+++ index.php
@@ -6,16 +6,20 @@
 
 <body>
   <form action="simpleform.php" method="post">
-    <input type="text" name="yourname" /> <br/>
+    <!-- <input type="text" name="yourname" /> <br/> -->
+    Email
+    <input type='text' class='form-control' id='requestedDate' name='requestedDate' placeholder='Email...'/>
+    Password
+    <input type="password" id="password" name="password" placeholder='Password...' required>
     <input type="submit" value="Submit" />
   </form>
   
-<?php
+<!-- <?php
 $str = "Hello world"; 
 if (isset($_POST['yourname']))
    $str = "You've entered ". $_POST['yourname'];
 echo $str;  
-?>
+?> -->
 
 </body>
 </html>   
