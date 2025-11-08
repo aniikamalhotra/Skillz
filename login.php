c09b933410b44932b836d217b19c49224d00a817
bhavyalboddu <bhavyalboddu@gmail.com>
2025-11-08 17:59:10 -0500

    added login page

---
+++ login.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">    
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Maintenance request form, a small/toy web app for ISP homework assignment, used by CS 3250 (Software Testing)">
+  <meta name="keywords" content="CS 3250, Upsorn, Praphamontripong, Software Testing">
+  
+  <title>Maintenance Services</title>
+ 
+</head>
+
+<body>  
+<div class="container">
+  <div class="row g-3 mt-2">
+    <div class="col">
+      <h1>Hi, welcome to Skillz!</h1>
+    </div>  
+  </div>
+  
+  <?php
+      include('connect-db.php');
+      include('request-db.php');
+  ?>
+
+  <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
+    <div class='mb-3'>
+        Email
+        <input type='text' class='form-control' 
+                id='requestedDate' name='requestedDate' 
+                placeholder='Email...' 
+                value="" />
+    </div>
+    <div class='mb-3'>
+        Password
+        <input type="password" id="password" name="password" placeholder='Password...' required>
+    </div>
+
+    <input type="submit" value="Submit" id="submitBtn" name="submitBtn" class="btn btn-dark"/>                  
+     
+    </div>  
+  </form>
+
+</div>
+
+
+<hr/>
+
+
+
+<br/><br/>
+
+<?php // include('footer.html') ?> 
+
+<!-- <script src='maintenance-system.js'></script> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+</html>
