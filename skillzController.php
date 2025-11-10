f981722e651c6f52bb08d388543fd951b73a26a1
bhavyalboddu <bhavyalboddu@gmail.com>
2025-11-10 14:41:55 -0500

    fixed one bug

--- skillzController.php
+++ skillzController.php
@@ -47,13 +47,13 @@ class skillzController {
 
     public function topicSelection() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if isset($_POST['music']) {
+            if (isset($_POST['music'])) {
                 header("Location: /?page=musicarticleslist")
                 exit;
-            } elseif isset($_POST['art']) {
+            } elseif (isset($_POST['art'])) {
                 header("Location: /?page=artarticleslist")
                 exit;
-            } elseif isset($_POST['sports']) {
+            } elseif (isset($_POST['sports'])) {
                 header("Location: /?page=sportarticleslist")
                 exit;
             }
