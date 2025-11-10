929d19c66b6c07d4b7a7ccc21b33b98025e831e5
bhavyalboddu <bhavyalboddu@gmail.com>
2025-11-10 15:22:40 -0500

    fixed another issue

--- index.php
+++ index.php
@@ -3,9 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-?>
 
-<?php
 include_once('connect-db.php');
 include_once('request-db.php');
 include_once('skillzController.php');
@@ -36,3 +34,5 @@ switch ($page) {
         $controller->home();
         break;
 }
+
+?>
