0114884987226655b99660a41560f6e5d0176129
bhavyalboddu <bhavyalboddu@gmail.com>
2025-11-10 18:11:00 -0500

    fixed issue maybe?

--- skillzController.php
+++ skillzController.php
@@ -69,6 +69,10 @@ class skillzController {
     }
 
     public function sportArticlesList() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            header("Location: /?page=sportarticleslist");
+            exit;
+        }
         include 'views/sportarticleslist.php';
     }
 
