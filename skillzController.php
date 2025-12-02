cf4b6934d9df48cca753d3ee3035bbb2e5f22947
bhavyalboddu <bhavyalboddu@gmail.com>
2025-12-02 16:04:04 -0500

    more testing

--- skillzController.php
+++ skillzController.php
@@ -96,7 +96,7 @@ class skillzController {
                     exit;
                 }
             } elseif (isset($_POST['like_on'])){
-                error_log("like_on triggered: " . $_POST['articleId']);
+                echo "<script>console.log('like_on clicked for article_id: " . $article_id . "');</script>";
                 $article_id = $_POST['articleId'] ?? null;
                  if ($article_id) {
                     upVote($_SESSION['user_id'], $article_id);
@@ -104,7 +104,7 @@ class skillzController {
                     exit;
                 }
             } elseif (isset($_POST['dislike_on'])){
-                error_log("dislike_on triggered: " . $_POST['articleId']);
+                echo "<script>console.log('dislike_on clicked for article_id: " . $article_id . "');</script>";
                 $article_id = $_POST['articleId'] ?? null;
                 if ($article_id) {
                     downVote($_SESSION['user_id'], $article_id);
@@ -112,7 +112,7 @@ class skillzController {
                     exit;
                 }
             } elseif (isset($_POST['like_off']) or isset($_POST['dislike_off'])){
-                error_log("like_off or dislike_off triggered: " . $_POST['articleId']);
+                echo "<script>console.log('like_off or dislike_off clicked for article_id: " . $article_id . "');</script>";
                 $article_id = $_POST['articleId'] ?? null;
                 if ($article_id) {
                     cancelVote($_SESSION['user_id'], $article_id);
